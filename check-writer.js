e161cd903e37a41c6ed6f51b6ae128577e9e6d62
joaovcastro <joao.castro@trialbee.com>
2018-09-04 20:03:21 +0200

    added recursion to algorithm

--- check-writer.js
+++ check-writer.js
@@ -1,67 +1,63 @@
 
 const unitsAndTeens = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-'ten', 'eleven', 'twelve', 'thriteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  'ten', 'eleven', 'twelve', 'thriteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
 const tens = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-const denoms = ['hundred', 'thousand'];
+const denoms = {
+  1000: ' thousand ',
+  100: ' hundred ',
+}
 
 
 function convertToWords(number) {
-	if (number < 20) {
-		return unitsAndTeens[number];
-	} else {
-		if (number < 100) {
-			const ten = Math.floor(number/10) - 2;
-			let res = tens[ten];
-			if (number % 10 !== 0) res += '-' + unitsAndTeens[number%10];
-			return res; 
-		} else {
-			const cent = Math.floor(number/100);
-			const lastTwoDigits = number % 100;
-			if (lastTwoDigits < 20)  {
-				return unitsAndTeens[cent] + ' hundred and ' + unitsAndTeens[lastTwoDigits];
-			} else {
-				const ten = tens[Math.floor(lastTwoDigits/10) - 2]
-				let res = unitsAndTeens[cent] + ' hundred and ' + ten;
-				if (number % 10 !== 0) res += '-' + unitsAndTeens[number % 10]
-				return res;
-			}
-		}
-	}
+  let numberInWords = '';
+
+  if (number === 0) return 'zero';
+
+  for (let i = 1000; i > 10; i /= 10) {
+    if (Math.floor(number / i) > 0) {
+      numberInWords += convertToWords(Math.floor(number / i)) + denoms[i];
+      number %= i;
+    }
+  }
+
+  if (number > 0) {
+    if (number < 20) numberInWords += unitsAndTeens[number];
+    else {
+      numberInWords += tens[Math.floor(number / 10) - 2];
+      if (number % 10 > 0) numberInWords += ' ' + unitsAndTeens[number % 10];
+    }
+  }
+
+  return numberInWords;
 }
 
 
 function main(number) {
-	console.log('\n\ninput', number);
-	let res = [];
-	let result = '';
-	// treat thousands
-	if (number > 1000) 
-		result +=  convertToWords(Math.floor(number / 1000)) + ' thousand ';
+  console.log('\n\ninput', number);
+  let result = '';
 
-	// treat units
-	result += convertToWords(Math.floor(number) % 1000);
+  result += convertToWords(Math.floor(number));
+  
+  result === 'one' ? result += ' euro' : result += ' euros';
+  
+  // treat cents
+  const decimals = (number + '').split('.');
+  if (decimals.length > 1 && decimals[1] !== "") {
+    let decimalsStr = decimals[1];
+    if (decimalsStr.length == 1)
+      decimalsStr += '0';
+    const decimalsNumber = parseInt(decimalsStr)
+    const decs = convertToWords(decimalsNumber);
+    result += ' and ' + decs + ' cent';
+    if (decs !== 'one') result += 's';
 
-	result === 'one' ? result += ' euro' : result += ' euros';
+  } else {
+    result += ' and zero cents';
+  }
 
-	// treat cents
-	const decimals = (number + '').split('.');
-	if (decimals.length > 1) {
-		let decimalsStr = decimals[1];
-		// console.log('str', decimalsStr, decimalsStr.length);
-		if (decimalsStr.length == 1)
-			decimalsStr += '0';
-
-		const decimalsNumber = parseInt(decimalsStr);
-		// console.log('decimalsNumber', decimalsNumber);
-		const decs = convertToWords(decimalsNumber);
-		decs === 'one' ? result += ' and ' + decs + ' cent' : result += ' and ' + decs + ' cents';
-	} else {
-		result += ' and zero cents';
-	}
-
-	console.log(result.slice(0,1).toUpperCase()+result.slice(1));
+  console.log(result.slice(0, 1).toUpperCase() + result.slice(1));
 }
 
 
@@ -69,4 +65,6 @@ main(112.10);
 main(874327.9);
 main(978);
 main(1.01);
-
+main(100);
+main(3000);
+main(12);
